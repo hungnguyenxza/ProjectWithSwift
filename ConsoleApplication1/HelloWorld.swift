020b48e75b4d46d487c659ce82ad0082a7e3cfe1
Xuân Hùng Nguyễn <hungnguyen@xuans-mac.local>
2019-08-10 01:26:39 +0700

    hello world

---
+++ ConsoleApplication1/HelloWorld.swift
@@ -0,0 +1,33 @@
+//
+//  HelloWorld.swift
+//  ConsoleApplication1
+//
+//  Created by Xuân Hùng Nguyễn on 8/9/19.
+//  Copyright © 2019 Xuân Hùng Nguyễn. All rights reserved.
+//
+
+import Foundation
+
+let pi = 3.14
+
+func sayHello(){
+    print("Hello, world!")
+    let `class` = "1";
+    print(`class`)
+}
+
+var xinChao = "Xin chao"
+
+func sayWelcome(){
+    //let hello = "123"
+//    print(xinChao)
+//    print(hello)
+    let a:Double = 12;
+    var b:Double = 21;
+    
+    print(a + b)
+    print(a - b)
+    print(a * b)
+    print(a / b)
+    
+}
