395ae5355a66d58dc6e487d8df75a5117709ea15
Xuân Hùng Nguyễn <hungnguyen@xuans-mac.local>
2019-08-09 23:40:41 +0700

    Initial Commit

---
+++ ConsoleApplication1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ConsoleApplication1
+//
+//  Created by Xuân Hùng Nguyễn on 8/9/19.
+//  Copyright © 2019 Xuân Hùng Nguyễn. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
